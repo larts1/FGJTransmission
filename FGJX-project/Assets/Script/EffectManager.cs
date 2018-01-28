f595f8a8a30522787af8947eb9d5b4e4556a3eb1
Lartsi <lauri@mantysaari.net>
2018-01-28 02:55:26 +0200

    Phases

--- FGJX-project/Assets/Script/EffectManager.cs
+++ FGJX-project/Assets/Script/EffectManager.cs
@@ -95,10 +95,16 @@ public class EffectManager : MonoBehaviour {
     }
 
     void StartDistortion() {
-        FindObjectOfType<AlpacaSound.RetroPixel>().horizontalResolution = 64;
-        Invoke( "StopDistortion", 0.5f );
+        FindObjectOfType<AlpacaSound.RetroPixel>().horizontalResolution = 640 / ( 3 - iter++ );
+        if ( iter == 3 ) {
+            Invoke( "StopDistortion", 0.5f );
+            iter = 0;
+        } else {
+            Invoke( "StartDistortion", 0.3f );
+        }
     }
 
+    int iter = 0;
     void StopDistortion() {
         FindObjectOfType<AlpacaSound.RetroPixel>().horizontalResolution = 640;
     }
