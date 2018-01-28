7d9260f81afed6552288b9ce89d46fda26baf633
Lartsi <lauri@mantysaari.net>
2018-01-28 14:39:01 +0200

    SpeedSound duration fix

--- FGJX-project/Assets/Script/AnimationAutoDestroy.cs
+++ FGJX-project/Assets/Script/AnimationAutoDestroy.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimationAutoDestroy : MonoBehaviour {
-    public float delay = 0f;
+    public float delay = 0.5f;
 
     // Use this for initialization
     void Start() {
-        Destroy( gameObject, this.GetComponent<Animation>().clip.length + delay );
-        Invoke( "beba", this.GetComponent<Animation>().clip.length + delay );
+        Destroy( gameObject, EffectManager.i.randomInterval ); //this.GetComponent<Animation>().clip.length + delay );
+
+        Invoke( "buba", GetComponent<Animation>().clip.length + delay );
+
+        Invoke( "beba", EffectManager.i.randomInterval );
     }
 
     void beba() {
         EffectManager.i.MuteMainAudio( false );
     }
 
+    void buba() {
+        GetComponentInChildren<Text>().enabled = false;
+    }
 }
